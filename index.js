688975a1a33e81d1472ea126a1a6f8256c0c3c42
Zuwaynib <zainabadeoje@gmail.com>
2025-03-10 22:58:51 +0100

    header: fixed navbar dark mode toggle  with javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+const toggleButton = document.querySelector('.dark-mode');
+const icon = toggleButton.querySelector('i');
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.replace('fa-sun', 'fa-moon'); // Change to moon icon
+    } else {
+        icon.classList.replace('fa-moon', 'fa-sun'); // Change back to sun icon
+    }
+});
