b15c2f71f543b1f86b3445e087ba0395e98456a7
Zuwaynib <zainabadeoje@gmail.com>
2025-03-11 21:03:53 +0100

    css: added dark mode styles for the body

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const toggleButton = document.querySelector('.dark-mode');
-const icon = toggleButton.querySelector('i');
+// const toggleButton = document.querySelector('.dark-mode');
+// const icon = toggleButton.querySelector('i');
 
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+// toggleButton.addEventListener('click', () => {
+//     document.body.classList.toggle('dark-mode');
     
-    if (document.body.classList.contains('dark-mode')) {
-        icon.classList.replace('fa-sun', 'fa-moon'); // Change to moon icon
-    } else {
-        icon.classList.replace('fa-moon', 'fa-sun'); // Change back to sun icon
-    }
-});
+//     if (document.body.classList.contains('dark-mode')) {
+//         icon.classList.replace('fa-sun', 'fa-moon'); // Change to moon icon
+//     } else {
+//         icon.classList.replace('fa-moon', 'fa-sun'); // Change back to sun icon
+//     }
+// });
+let darkMode = document.querySelector(".dark-mode");
