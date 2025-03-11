745ad607861c8e6110ec275cbf4a92eb0572067c
Zuwaynib <zainabadeoje@gmail.com>
2025-03-11 21:26:53 +0100

    about: initialized the about section

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const darkMode = document.querySelector(".dark-mode");
 if (darkMode) {  
     if (localStorage.getItem("mode") === "Dark Mode") {
         document.body.classList.add("dark");
-        darkMode.textContent = "Light Mode";
+        darkMode.textContent = "Dark Mode";
     }
 
     darkMode.addEventListener("click", () => {
         document.body.classList.toggle("dark");
         const isDarkMode = document.body.classList.contains("dark");
-        darkMode.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
-        localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
+        darkMode.textContent = isDarkMode ? "Dark Mode" : "Light Mode";
+        localStorage.setItem("mode", isDarkMode ? "Light Mode" : "Dark Mode");
     });
 }
